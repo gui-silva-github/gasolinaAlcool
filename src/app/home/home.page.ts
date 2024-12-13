af117125d4873272bb8453785c7e433547d4315f
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-12-13 12:10:48 -0300

    Typescript do app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  public logo: string = "../../assets/logo.png"
+
+  public imagem: string = ""
+
+  public resultado: string = "Resultado"
+
+  public precoAlcool: string = ""
+  public precoGasolina: string = ""
 
   constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  public calcular(): void{
+
+    if(this.precoAlcool && this.precoGasolina){
+
+      let pAlcool = parseFloat(this.precoAlcool)
+
+      let pGasolina = parseFloat(this.precoGasolina)
+
+      let res = (pAlcool/pGasolina)
+
+      if(res>=0.7){
+        this.resultado = "Melhor utilizar gasolina!"
+        this.imagem = "../../assets/gasolina.png"
+      } else {
+        this.resultado = "Melhor utilizar álcool!"
+        this.imagem = "../../assets/alcool.png"
+      }
+
+    } else{
+      this.resultado = "Preencha corretamente os campos."
+    }
+
+  }
 
 }
